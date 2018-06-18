123c3781e6fc622f657198716e804963196716ce
mkil5339 <madhura.killedar@sydney.edu.au>
2018-06-18 14:22:09 +1000

    change predictors; increase steps

--- GPplus2/settings.py
+++ GPplus2/settings.py
@@ -25,7 +25,7 @@ outmcmc = outpath # add any subdirectory if required
 ####### Define options for analysis and visualisation
 
 split_traintest = 0.2 # splits data in train and test data, provide fraction of test
-simulate = False # Creates simulated data with 2dim spatial coordinates and 2 features and then runs code on this data.
+simulate    = False # Creates simulated data with 2dim spatial coordinates and 2 features and then runs code on this data.
 calc_center = False # Calculates centroids from polygons and converts from Lat/Lng to cartesian with center coord below.
                    # If calc_center = False, coordinates have to be provided in main input file [fname_input]
                    # using the two column names "centroid_x" and "centroid_y"
@@ -35,9 +35,9 @@ create_maps = False # creates interactive html maps of crime results, not includ
 # GP setup
 kernelname = 'expsquared' # choose from: 'expsquared', 'matern32' or 'rationalq' (Rational Quadratic)
 # MCMC setup; see emcee documentation for more details (http://dfm.io/emcee/current/)
-nwalkers = 20 # Number of walkers, recommended at least 2 * Nparameter + 1 (recommended more)
-niter = 1500  # Number of iterations, recommended at least 500
-nburn = 2000  # Number of iterations, recommended at least 20% of niter, check sampler chains for convergence.
+nwalkers = 24 # Number of walkers, recommended at least 2 * Nparameter + 1 (recommended more)
+niter = 2500  # Number of iterations, recommended at least 500
+nburn = 1500 # Number of iterations, recommended at least 20% of niter, check sampler chains for convergence.
 nfold_cross = 1 # Number of x-fold for cross-validation for test-train sets; set to 1 for only one run 
 #(Notes: test first with nfold-cross = 1; computational time ~ nfold_cross; also not all plotting availabable yet for nfold_cross>1)
 use_log = False # select false if input features need NOT to be converted into log-space.
@@ -57,24 +57,28 @@ target_desc = 'Log Precipitation' # for plot axes labels
 x_feature_names = ['Coal Deposits',
                    'Evaporites Deposits',
                    'Glacial Deposits',
-                   'Elevation',
-                   'Dist to Shore']
-                   #'Sqrt Dist',
+                   #'Elevation',
+                   'Sqrt Elevation',
+                   #'Dist to Shore'
+                   'Sqrt Dist',
                    #'ASinh Elevation Sc50',
                    #'Sin Angle Shore',
-                   #'Cos Angle Shore']
+                   #'Cos Angle Shore'
+                   ]
 
 
 # Optionally provide additional description names for features, replace names accordingly:
 x_feature_desc = ['Coal Deposits',
                   'Evaporites Deposits',
                   'Glacial Deposits',
-                  'Elevation',
-                  'Distance to Shore']
-                  #'Sqrt Distance',
+                  #'Elevation',
+                  'Sqrt Elevation',
+                  #'Distance to Shore'
+                  'Sqrt Distance',
                   #'ASinh Elevation Sc50',
                   #'Eastern  shore (sin)',
-                  #'Northern shore (cos)']
+                  #'Northern shore (cos)'
+                  ]
                   
 
 #######################################
