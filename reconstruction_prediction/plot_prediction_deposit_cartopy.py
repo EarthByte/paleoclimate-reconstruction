6e2bf13b4b1f5fb1bed422dd38ae3876ae2ac268
natbutter <natbutter@gmail.com>
2020-01-28 12:19:14 +1100

    Fixed cartopy plotting with new outputs

--- reconstruction_prediction/plot_prediction_deposit_cartopy.py
+++ reconstruction_prediction/plot_prediction_deposit_cartopy.py
@@ -7,52 +7,18 @@
 # ## date   : 20/07/2018
 # 
 
-from __future__ import print_function
 import sys
 import os
 import numpy as np
 import pandas as pd
 from math import asin, acos, sqrt, sin, cos, radians, degrees, asinh
 from matplotlib import pyplot as plt
-import seaborn as sns
+import matplotlib
 import csv
 import cartopy.crs as ccrs
+matplotlib.use('Agg')
 
-
-from mpl_toolkits.basemap import Basemap
-from scipy.ndimage import filters
-from scipy.ndimage import interpolation
-import cv2
-from matplotlib.patches import Polygon
-#from matplotlib.collections import PatchCollectioninput
-
-import shapefile
-
-def readTopologyPlatepolygonFile(filename):
-    '''
-    Reads shapefiles and returns the all the data fields
-    '''
-    shapeRead = shapefile.Reader(filename)
-
-    recs    = shapeRead.records()
-    shapes  = shapeRead.shapes()
-    fields  = shapeRead.fields
-    Nshp    = len(shapes)
-    
-    return(recs,shapes,fields,Nshp)
-
-
-from matplotlib import rc
-font = {'family': 'serif',
-        'serif' : ['Palatino'],
-        'weight': 'bold',
-        'size'  : 20}
-rc('font', **font)
-rc('text', usetex=True)
-from matplotlib import rcParams
-fig_dpi = 150
-rcParams['figure.dpi']= fig_dpi
-
+import seaborn as sns
 print("Hello Prediction Plotting")
 
 
@@ -63,43 +29,37 @@ if(len(sys.argv)!=4):
     sys.exit('Usage: python plot_prediction.py <input file> <one-word subject of folder where data input resides  and outputs will be kept>')
 
  
-
 predict_folder = str(sys.argv[1])
-
-
 predict_filename = str(sys.argv[2])
-
-
 subject = str(sys.argv[3])
 
 directory_plot =  predict_folder+'/'+subject
 
+if not os.path.exists(directory_plot):
+    os.makedirs(directory_plot) 
+
+directory_plot =  predict_folder+'/'+subject + "/map_prediction_"
 
 if not os.path.exists(directory_plot):
     os.makedirs(directory_plot) 
 
-
-directory_plot =  predict_folder+'/'+subject + "/map_prediction_"
-
+directory_plot =  predict_folder+'/'+subject + "/map_earth_"
 
 if not os.path.exists(directory_plot):
     os.makedirs(directory_plot) 
 
 directory_plot =  predict_folder+'/'+subject + "/map_prediction_uncert_"
 
-
 if not os.path.exists(directory_plot):
     os.makedirs(directory_plot) 
 
 directory_plot =  predict_folder+'/'+subject + "/map_actual_"
 
-
 if not os.path.exists(directory_plot):
     os.makedirs(directory_plot) 
 
-
-
 directory_plot =  predict_folder+'/'+subject
+
 
 print(predict_filename , ' predict_filename')
 
@@ -108,15 +68,12 @@ prediction_file = predict_folder +'/' + predict_filename  # example only
 print("Prediction file path: %s"%(prediction_file))
  
   
-  
+#Read in the data  
 predictions = pd.read_csv(prediction_file, header=None)
  
   
 latitudes     = predictions.loc[:,0]
 longitudes    = predictions.loc[:,1]
-
-#xt    = predictions.loc[:,3]
- 
 
 
 # ## SET UP GRIDS
@@ -124,7 +81,7 @@ lon_min = -180.
 lon_max =  180.
 lat_min =  -90.
 lat_max =   90.
-lon_spacing = 3. #2.51
+lon_spacing = 3 #2.51
 
 lon_coords = np.arange(lon_min,lon_max-0.5*lon_spacing,lon_spacing) # left edge of grid
 nlonbins = len(lon_coords)
@@ -141,7 +98,7 @@ for ilat in np.arange(nlatbins-1):
     lat_spacing[ilat] = lat_coords[ilat+1] - lat_coords[ilat]
 lat_spacing[nlatbins-1] = 90. - lat_coords[nlatbins-1]
 print("Check central latitude bin-edges ...")
-print(lat_coords[nlatbins/2-1:nlatbins/2+2])
+print(lat_coords[int(nlatbins/2-1):int(nlatbins/2+2)])
 # remove polar grid-bins
 nlatbins = nlatbins-2
 lat_coords = lat_coords[1:-1]
@@ -149,9 +106,7 @@ lat_spacing = lat_spacing[1:-1]
 
 
 
-
 # # PRECIPITATION
-
 map_predict_actual = np.zeros((nlatbins,nlonbins))
 map_predict_mean = np.zeros((nlatbins,nlonbins))
 map_predict_low  = np.zeros((nlatbins,nlonbins))
@@ -162,7 +117,6 @@ list_mean = []
 list_low  = []
 list_high = []
 list_unct = []
-
 list_actual = []
 
 if subject == 'coal':
@@ -170,29 +124,20 @@ if subject == 'coal':
     mean_col  = 6 
     high_col =  12
     low_col =  9
-
     print(subject, ' is subject')
-
 
 if subject == 'glacial':
     actual_col =   17
     mean_col  =  8
     high_col = 14
     low_col =  11
-
-
     print(subject, ' is subject')
-
-
-
 
 if subject == 'evaporites':
     actual_col = 16
     mean_col  =  7  
     high_col =  13
     low_col =  10
-
-
     print(subject, ' is subject')
 
 
@@ -244,74 +189,42 @@ for ilon,lon_low in enumerate(lon_coords):
 
 
 lons, lats = np.meshgrid(lon_coords,lat_coords)
+actual_data = predictions[predictions.loc[:,actual_col]>0].loc[:,actual_col].values
+actual_lat = predictions[predictions.loc[:,actual_col]>0].loc[:,0].values
+actual_lon = predictions[predictions.loc[:,actual_col]>0].loc[:,1].values
+map_predict_mean[map_predict_mean==0]=np.nan
 
-
-
-
-
+# set up a map
 fig = plt.figure(figsize=(16,12),dpi=150)
-ax = fig.add_subplot(111)
+ax = plt.axes(projection=ccrs.Mollweide())
+ax.set_global()
+ax.gridlines(linewidth=0.1)
 
-pmap = Basemap(projection='moll', lat_0=0, lon_0=0, resolution='l')
-pmap.drawmapboundary(fill_color='white')
-
-#Load in plate polygons for plotting
-#topologyFile='/Users/nbutter/Projects/GEO/Eocene/paleoclimate-reconstruction/data/PaleomagneticReferenceFrame/reconcoast/reconstructed_'+time1+'.00Ma.shp'
-#[recs,shapes,fields,Nshp]=readTopologyPlatepolygonFile(topologyFile)
-#patches   = []
-'''for i, nshp in enumerate(range(Nshp)):
-    #These are the plates that cross the dateline and cause 
-        #banding errors
-        polygonShape=shapes[nshp].points
-        poly=np.array(polygonShape)
-        testnum=max(poly[:,0])-min(poly[:,0])
-        if testnum < 180.0:
-            #lon_unwrapped = np.rad2deg(np.unwrap(np.deg2rad(poly[:,0])))
-            #print(poly[:,0])
-            #print(lon_unwrapped)
-            a=pmap(poly[:,0],poly[:,1])
-            #a=pmap(lon_unwrapped,poly[:,1])
-            b=np.array([a[0],a[1]])
-            patches.append(Polygon(b.T,closed=False,color='dimgrey'))
-            #pc = PatchCollection(patches, match_original=True, edgecolor='k', linewidths=1., zorder=2)
-            #ax.add_collection(pc)
-            #xs, ys = pmap(poly[:,0], poly[:,1])
-            #pmap.plot(xs, ys, 'k',zorder=1)
-        else:
-            print(testnum, recs[nshp][8],nshp)
-        
-
-pc = PatchCollection(patches, color='dimgrey', linewidths=1, zorder=1)
-ax.add_collection(pc)'''
-        
+#Create the varible to plot
 intensity = np.ma.masked_where(np.isnan(map_predict_mean), map_predict_mean)
-im1 = pmap.pcolormesh(lons,-lats,intensity,shading='flat',cmap=plt.cm.gist_earth_r,latlon=True)
 
+#Plot on map
+mapscat=plt.pcolormesh(lons,-lats,intensity,shading='flat',cmap=plt.cm.gist_earth_r,transform=ccrs.PlateCarree())
+#Add additional points
+line=ax.scatter(actual_lon,actual_lat,transform=ccrs.PlateCarree(),label='Actual Deposits')
 
+#Add the legend, colorbar and some text
+plt.legend(loc=4,prop={'size': 15})
 
-#pip install matplotlib==2.0.2  (this resolved error: MatplotlibDeprecationWarning: The axesPatch function was deprecated in version 2.1. Use Axes.patch instead)
+#Get the current time from the filename
+time1=predict_filename.split('era')
+time2=time1[1].split('results')
+figtime=time2[0]
+print("fig time=",figtime)
+plt.text(14000000,7000000,str(figtime)+" Ma",size=20)
 
+cbar=plt.colorbar(mapscat, ax=ax, orientation="vertical", pad=0.05, fraction=0.015, shrink=0.5)
+cbar.set_label('Prediction for '+subject,labelpad=10,size=20)
 
-cb = pmap.colorbar(im1,"right", size="3%", pad="5%",ticks=[0,1],fraction=0.001)
-plt.clim(0,1)
-cb.set_label('Prediction for '+subject,labelpad=10,size=20)
-
-
-xh, yh = pmap(actual_lon, actual_lat)
-line,=pmap.plot(xh,yh,'ob',label='Actual '+subject)
-plt.legend(loc=4,prop={'size': 20})
-#pmap.imshow(intensity,extent=[-180,180,-90,90],origin='upper',interpolation='bilinear',cmap=plt.cm.binary)
-plt.text(33000000,16000000,time1+" Ma",size=20)
-
-
-pmap.drawmeridians(np.arange(0, 360, 60),color='grey',dashes=[1,0],linewidth=0.2)
-pmap.drawparallels(np.arange(-90, 90, 45),color='grey',dashes=[1,0],linewidth=0.2)
-
-
+fig.savefig( directory_plot+"/map_earth_/"+ predict_filename+".pdf", pad_inches=0.6)
+fig.clf()
 
 plt.show()
-
-
 
 
 print("minimum mean prediction is "+str(np.ma.array(list(map_predict_mean),mask=mask_exclude).min()))
